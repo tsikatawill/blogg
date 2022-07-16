d8421e81bddd53054d749b4070da8ba9aef8f16a
William M. Tsikata <tsikatawill@gmail.com>
2022-07-16 12:08:06 +0000

    cleanup app js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import FeaturedPostsSection from './components/FeaturedPostsSection'
 import Header from './components/Header'
 import StoryPostCard from './components/StoryPostCard'
-import useUser from './hooks/getUser'
 
 function App() {
-  // console.log(useUser('LWmCmpnUrXTYu37Mrj0jlr5NOeC2'))
   return (
     <>
       <Header />
       <div className="container py-5">
         <FeaturedPostsSection />
+
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 mt-10">
-          <StoryPostCard />
-          <StoryPostCard />
-          <StoryPostCard />
           <StoryPostCard />
           <StoryPostCard />
           <StoryPostCard />

75f83753c0fc98e1d94ce6f1a67a8d62f551eca8
William M. Tsikata <tsikatawill@gmail.com>
2022-07-16 13:36:48 +0000

    cleanup unused imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import AllStoriesSection from './components/AllStoriesSection'
 import FeaturedPostsSection from './components/FeaturedPostsSection'
 import Header from './components/Header'
-import StoryPostCard from './components/StoryPostCard'
 import useBlogPosts from './hooks/useBlogPosts'
 
 function App() {

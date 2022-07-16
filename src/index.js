8af7e2ca43c02e9aef91237519b1c33ae17692bf
William M. Tsikata <tsikatawill@gmail.com>
2022-07-16 02:49:25 +0000

    add login page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
+import Login from './pages/Login'
+import MainLayout from './layouts/MainLayout'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import MainLayout from './layouts/MainLayout'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -18,6 +19,7 @@ root.render(
             </MainLayout>
           }
         />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>

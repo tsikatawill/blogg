5f5568c8895075374af74bbfbe8f7d2801ec1a35
William M. Tsikata <tsikatawill@gmail.com>
2022-07-16 12:08:49 +0000

    add toast container to index js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 import './index.css'
+import 'react-toastify/dist/ReactToastify.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
 import Login from './pages/Login'
 import MainLayout from './layouts/MainLayout'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ToastContainer } from 'react-toastify'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <MainLayout>
-              <App />
-            </MainLayout>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <>
+    <React.StrictMode>
+      <ToastContainer limit={3} theme={localStorage.theme} />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <MainLayout>
+                <App />
+              </MainLayout>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  </>
 )

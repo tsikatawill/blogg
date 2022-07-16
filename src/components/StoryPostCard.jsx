8eab926b57cb9f08aceecd5e134638997d3853d5
William M. Tsikata <tsikatawill@gmail.com>
2022-07-16 12:16:39 +0000

    add hover effect to post display image

--- src/components/StoryPostCard.jsx
+++ src/components/StoryPostCard.jsx
@@ -3,11 +3,11 @@ import { HiOutlineBookmark } from 'react-icons/hi'
 export default function StoryPostCard() {
   return (
     <div className="rounded-md h-[30rem] bg-white dark:bg-slate-500 border-2 dark:border-slate-500">
-      <div className="h-1/2">
+      <div className="h-1/2 overflow-hidden">
         <img
           src="https://uploads.linear.app/5b440f6e-6aed-4159-b0ed-f69d7442da54/5c69faab-ebd4-4ed0-9660-db6a80f5e3b7/66304491-cc7b-4eeb-bcee-2333a1b69ae8"
           alt="profile"
-          className="h-full object-cover w-full"
+          className="h-full object-cover w-full hover:scale-110 duration-150 ease-in"
         />
       </div>
       <div className="content p-4 lg:p-7 flex flex-col  justify-between">

c72b0e3498eb5d8e3f16bae56a3b5a0b48843181
William M. Tsikata <tsikatawill@gmail.com>
2022-07-16 12:12:00 +0000

    modify header
    move styles to shared css module file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,11 @@
 import { AiOutlineRocket } from 'react-icons/ai'
+import styles from '../styles/shared.module.css'
 
 export default function Header() {
   return (
     <header
-      className="h-screen sm:h-[400px]"
+      className={`${styles.homepageHeader} h-screen sm:h-[400px]`}
       data-testid="homepage-header"
-      style={{
-        background:
-          'linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.7)), url(https://uploads.linear.app/5b440f6e-6aed-4159-b0ed-f69d7442da54/5c69faab-ebd4-4ed0-9660-db6a80f5e3b7/66304491-cc7b-4eeb-bcee-2333a1b69ae8) center center/cover no-repear',
-        backgroundAttachment: 'fixed',
-      }}
     >
       <div className="container grid h-full place-content-center">
         <div className="flex items-center text-white">

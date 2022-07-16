ec608e5e54e11d95686113aaa1289756c9d3431b
William M. Tsikata <tsikatawill@gmail.com>
2022-07-16 12:18:08 +0000

    modify login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,21 @@
 import LoginForm from '../components/LoginForm'
 import Logo from '../components/Logo'
+import SignupForm from '../components/SignupForm'
 import ThemeToggler from '../components/ThemeToggler'
+import { useState } from 'react'
 
 export default function Login() {
+  const [selectedForm, setSelectedForm] = useState({
+    name: 'login',
+    style: 'border-b-2 border-purple-500 pb-2 text-purple-500 cursor-pointer',
+  })
+
   return (
     <main className="grid place-content-center min-h-screen">
       <div className="fixed bottom-10 right-10">
         <ThemeToggler />
       </div>
+
       <div className="container">
         <div className="bg-white dark:bg-slate-900 w-[20rem] border border-gray-500 dark:border-gray-700 shadow-md shadow-slate-400 dark:shadow-stone-900 rounded-md py-10">
           <div className="header flex justify-center">
@@ -15,15 +23,29 @@ export default function Login() {
           </div>
 
           <div className="nav flex justify-between text-center mt-5 mb-2">
-            <div className="nav-item flex-1 border-b-2 border-purple-500 pb-2 text-purple-500 cursor-pointer">
+            <div
+              className={`nav-item flex-1 cursor-pointer ${
+                selectedForm.name === 'login' && selectedForm.style
+              }`}
+              onClick={() =>
+                setSelectedForm({ ...selectedForm, name: 'login' })
+              }
+            >
               Log in
             </div>
-            <div className="nav-item flex-1 border-b-2 hover:border-purple-500 pb-2 hover:text-purple-500 cursor-pointer">
-              Sign Up
+            <div
+              className={`nav-item flex-1 cursor-pointer ${
+                selectedForm.name === 'signup' && selectedForm.style
+              }`}
+              onClick={() =>
+                setSelectedForm({ ...selectedForm, name: 'signup' })
+              }
+            >
+              Sign up
             </div>
           </div>
 
-          <LoginForm />
+          {selectedForm.name === 'login' ? <LoginForm /> : <SignupForm />}
         </div>
       </div>
     </main>

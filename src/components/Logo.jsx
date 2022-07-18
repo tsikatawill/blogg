c61190b6b9e72182721e5df553222a4499fc8d62
William M. Tsikata <tsikatawill@gmail.com>
2022-07-18 04:48:49 +0000

    modify logo responsiveness

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,13 +3,12 @@ import { AiOutlineRocket } from 'react-icons/ai'
 export default function Logo() {
   return (
     <span className="flex gap-2 items-center">
-      <AiOutlineRocket
-        size={40}
-        className="rotate-45 hover:rotate-[410deg] duration-1000 text-purple-600"
-      />
+      <AiOutlineRocket className="rotate-45 hover:rotate-[410deg] duration-1000 text-purple-600  text-3xl md:text-4xl" />
       <span className="flex flex-col items-start">
         <span className="text-2xl text-purple-600 font-bold">Blogg</span>
-        <small className="text-[10px] leading-3">Where the world blogs</small>
+        <small className="text-[10px] leading-3 hidden sm:block">
+          Where the world blogs
+        </small>
       </span>
     </span>
   )

b02589d7fb3577fac7003d8f3d14c9875fe744af
William M. Tsikata <tsikatawill@gmail.com>
2022-07-18 04:48:18 +0000

    modify header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,15 @@ import styles from '../styles/shared.module.css'
 export default function Header() {
   return (
     <header
-      className={`${styles.homepageHeader} h-screen sm:h-[400px]`}
+      className={`${styles.homepageHeader} h-[calc(100vh-4rem)] md:h-[400px]`}
       data-testid="homepage-header"
     >
       <div className="container grid h-full place-content-center">
-        <div className="flex items-center text-white">
+        <div className="flex items-center flex-col md:flex-row text-white">
           <span className="rotate-45">
-            <AiOutlineRocket size={100} />
+            <AiOutlineRocket className="text-8xl md:text-9xl" />
           </span>
-          <span className="text-7xl font-bold">BLOGG</span>
+          <span className="text-5xl sm:text-7xl font-bold">BLOGG</span>
         </div>
       </div>
     </header>
